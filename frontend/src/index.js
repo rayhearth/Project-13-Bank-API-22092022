160bcc72fa33e7d7d61a4a96c8a13f8d5781176a
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-09-29 12:43:32 +0200

    creation caller.services

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,9 +20,9 @@ const queryClient = new QueryClient({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}></QueryClientProvider>
-    <App />
-  </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode >
 );
 
