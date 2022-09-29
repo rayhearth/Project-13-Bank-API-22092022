5ef63b73a9b60575b996feb1b0a9813ad55fa3e9
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-09-29 12:34:54 +0200

    install react-quary

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import reportWebVitals from './reportWebVitals';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnmount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}></QueryClientProvider>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
+  </React.StrictMode >
 );
 
 // If you want to start measuring performance in your app, pass a function
