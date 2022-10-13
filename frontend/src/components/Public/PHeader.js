0833343f735b04b34ccdd5efac9be0a516be218c
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-10-13 11:01:57 +0200

    maj link index et icon

--- frontend/src/components/Public/PHeader.js
+++ frontend/src/components/Public/PHeader.js
@@ -5,14 +5,14 @@ const PHeader = () => {
     return (
         <header>
             <nav className="main-nav">
-                <a className="main-nav-logo" href="./index.html">
+                <Link to='/' className="main-nav-logo">
                     <img
                         className="main-nav-logo-image"
                         src={process.env.PUBLIC_URL + '/assets/img/argentBankLogo.png'}
                         alt="Argent Bank Logo"
                     />
                     <h1 className="sr-only">Argent Bank</h1>
-                </a>
+                </Link>
                 <div>
                     <Link to='/login' className="mainNavSignIn" >
                         <i className="fa fa-user-circle"></i>
