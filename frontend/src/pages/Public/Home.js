75ee046dadbc2ca88faecd30d053eae579aa3680
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-11-01 16:13:22 +0100

    correction style home

--- frontend/src/pages/Public/Home.js
+++ frontend/src/pages/Public/Home.js
@@ -6,11 +6,14 @@ const Home = () => {
 
         <div className="hero">
             <section className="hero-content">
-                <h2 className="sr-only">Promoted Content</h2>
-                <p className="subtitle">No fees.</p>
-                <p className="subtitle">No minimum deposit.</p>
-                <p className="subtitle">High interest rates.</p>
-                <p className="text">Open a savings account with Argent Bank today!</p>
+                <div className="text-content">
+                    <h2 className="sr-only">Promoted Content</h2>
+                    <p className="subtitle">No fees.</p>
+                    <p className="subtitle">No minimum deposit.</p>
+                    <p className="subtitle">High interest rates.</p>
+                    <p className="text">Open a savings account with Argent Bank today!</p>
+
+                </div>
             </section>
             <Features />
         </div>
